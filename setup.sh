f55a0d8f133209de64b0c522cf8622b2579d7a2f
Bruno Siqueira <92235799+lvl1rat@users.noreply.github.com>
2022-09-15 15:57:24 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-echo
-echo "---Installing Content_Check---"
-echo
-echo "Copying archives..."
-cp content_check.sh ~/.content_check.sh
-echo "Setting up shortcut..." 
-echo "alias content_check='~/.content_check.sh'" >> ~/.bash_aliases
-source ~/.bash_aliases
-echo "Setting up permissions"
-chmod 555 ~/.content_check.sh
-echo
-echo "-------Setup Finished.-------"
-echo
-echo "See README"
-echo
+while getopts 'iu' OPTION; do
+  case "$OPTION" in
+	i)
+		echo
+		echo "---Installing Content_Check---"
+		echo
+		echo "Copying archives..."
+		cp content_check.sh ~/.content_check.sh
+		echo "Setting up shortcut..." 
+		echo "alias content_check='~/.content_check.sh'" >> ~/.bashrc
+		echo "Setting up permissions..."
+		chmod 555 ~/.content_check.sh
+		source ~/.bashrc
+		echo
+		echo "-------Setup Finished.-------"
+		echo
+		echo "See README"
+		echo
+	;;
+	u)
+		echo
+		echo "---Removing Content-Check and all dependencies---"
+		echo
+		unalias content_check
+		rm -f ~/.content_check.sh
+		echo
+		echo "---------------Unistall Finished.---------------"
+		echo
+	;;
+  esac
+done
+shift "$(($OPTIND -1))"
